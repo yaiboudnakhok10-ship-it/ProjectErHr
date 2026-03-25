295d1302d7e6ff526b1034e9c7efd8a44af5f22b
yaiboudnakhok10-ship-it <yaiboudnakhok10@gmail.com>
2026-03-25 11:27:03 +0700

    ERHR

---
+++ ERHR/frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Test API</h1>
+    <button @click="testAPI">เชื่อมต่อ Backend</button>
+    <pre>{{ result }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import api from './services/api.ts'
+
+const result = ref(null)
+
+const testAPI = async () => {
+  try {
+    const res = await api.get('/employees')
+    result.value = res.data
+  } catch (err) {
+    result.value = err.message
+  }
+}
+</script>
